717aa2d589659eb54f4cd8b5bc009eba25c7d318
MUXICORP\gfuchs <gfuchs>
2019-10-11 18:42:53 -0300

    - Dojo III;

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,25 +6,67 @@ import { JuliusButton, DojoButton } from './atoms/button'
 import MainTemplate from './templates/main'
 import CustomPaginationActionsTable from "./tableExample"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <MainTemplate />
+const App = () => {
 
-        <JuliusButton />
-        <DojoButton>TESTE</DojoButton>
-        <CustomPaginationActionsTable 
-          data={{
-                "titles": ["title", "title2"],
-                "rows": [{"title": "teste", "title2": "aaaa"}, 
-                       {"title": "teste", "title2": "aaaa"}, 
-                       {"title": "teste", "title2": "aaaaaa"}]
-            }}
-          />
-      </div>
-    );
+  const [tableRows, setTableRows] = useState([]);
+  
+  const getFinancialCycles = () => {
+    console.log('Capivara');
+    var baseUrl = 'http://107.23.28.111:8083/v1/financial_cycles/search';
+    var contentType = 'application/json';
+    var authorization = 'eyJraWQiOiJVU0VSIiwidHlwIjoiSldUIiwiYWxnIjoiSFM1MTIifQ.eyJzbHVnVXNlciI6IjY5RTgzNEIyQjRCMDRFMUNCNjI1QjU5RkY1MEQzREU0IiwiaXNzIjoiZ2Z1Y2hzK2dpZ2FudGVAbXV4aS5jb20uYnIiLCJwZXhwIjoxNjE5ODM4MDAwLCJleHAiOjE1NzA4NTExMDB9.Y2GpvL-Vof-rkCPAxESmPxg2iQKWcuM6yoRA2XLhyWc-d2F7Oko90mUCw8KpNr_i7wP6rv03ks4j09wBYnaCNw';
+    var xCustomer = '2B70D53FE85541188816CAB2497586D4';
+
+    var search_parameters = {
+        limit: 20,
+        offset: 0,
+        order_by: '-dtInsert',
+        filter: 'cycleType__nin:ACH_CHECK:PRE_AUTHORIZATION_EXPIRATION'
+    };
+ 
+    var url = new URL(baseUrl);
+    if(search_parameters !== null && search_parameters !== undefined) {
+        Object.keys(search_parameters)
+        .forEach(key => url.searchParams.append(key, search_parameters[key]));
+    }    
+
+    const request_config = {
+        headers: {
+            'Accept': contentType,
+            'Content-Type': contentType,
+            'X-Customer': xCustomer,
+            'Authorization': authorization
+        }
+    };
+
+
+    fetch(url, request_config)
+      .then(result => result.json())
+      .then((result) => {
+        setTableRows(result.objects)
+      });
+
   }
+
+  useEffect(() => {
+    getFinancialCycles();
+  })
+
+  return (
+    <div className="App">
+      <MainTemplate />
+
+      <JuliusButton />
+      <DojoButton onClick={getFinancialCycles}>TESTE</DojoButton>
+      <CustomPaginationActionsTable 
+        data={{
+              "titles": ["dtInsert", "gatewayStan", "cycleType", "amount", "cycleStatus"],
+              "rows": tableRows
+          }}
+        />
+    </div>
+  );
+  
 }
 
 export default App;
--- src/atoms/button/dojoButton.js
+++ src/atoms/button/dojoButton.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const DojoButton = (props) => {
     return (
-        <button className="btn btn-primary">{ props.children }</button>   
+        <button 
+        className="btn btn-primary"
+        onClick={props.onClick}>
+            { props.children }
+        </button>   
     );
 }
+
+
 
 export default DojoButton;
